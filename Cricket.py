d3ce9ec0a1237234d1dcd14e8ea6f509bfcea10c
rawsand <raw.sand85@gmail.com>
2026-06-14 20:23:25 +0530

    Update Cricket.py

--- Cricket.py
+++ Cricket.py
@@ -2,12 +2,16 @@ import requests
 
 url = "https://la.drmlive.net/tp/playlist"
 
-r = requests.get(url, timeout=30)
+headers = {
+    "User-Agent": "OTT Navigator"
+}
+
+r = requests.get(url, headers=headers, timeout=30)
 
 with open("8b249zhj3vg65us_sports.m3u", "w", encoding="utf-8") as f:
     f.write(f"Status: {r.status_code}\n")
     f.write(f"Content-Type: {r.headers.get('Content-Type')}\n")
-    f.write(f"Content-Length: {len(r.text)}\n\n")
+    f.write(f"Final URL: {r.url}\n\n")
     f.write(r.text)
 
-print("Output saved to response.txt")
+print("Saved to ott_response.txt")
