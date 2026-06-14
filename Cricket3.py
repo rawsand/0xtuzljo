83c277f8378abb52ae5072182f073cda4e972a49
rawsand <raw.sand85@gmail.com>
2026-06-14 21:10:03 +0530

    Update Cricket3.py

--- Cricket3.py
+++ Cricket3.py
@@ -8,7 +8,7 @@ headers = {
 
 r = requests.get(url, headers=headers, timeout=30)
 
-with open("8b249zhj3vg65us_sports.m3u", "w", encoding="utf-8") as f:
+with open("8b249zhj3vg65us_sports1.m3u", "w", encoding="utf-8") as f:
     f.write(f"Status: {r.status_code}\n")
     f.write(f"Content-Type: {r.headers.get('Content-Type')}\n")
     f.write(f"Final URL: {r.url}\n\n")
