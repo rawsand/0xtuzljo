0c125a3d21d8d7dd2e5be855376b6e24d5a85583
rawsand <raw.sand85@gmail.com>
2026-06-14 20:13:09 +0530

    Update Cricket.py

--- Cricket.py
+++ Cricket.py
@@ -5,10 +5,9 @@ m3u_url = "https://la.drmlive.net/tp/playlist"
 output_file = "8b249zhj3vg65us_sports.m3u"
 
 result = subprocess.run(
-    ["curl", "-L", "-s", m3u_url],
+    ["curl", "-L", "-I", "https://la.drmlive.net/tp/playlist"],
     capture_output=True,
-    text=True,
-    check=True
+    text=True
 )
 
 with open(output_file, "w", encoding="utf-8") as f:
